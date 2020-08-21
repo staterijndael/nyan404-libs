5feaa01fd463d28338c4e3509ed8b459968ba37a
Oringik <zikalol2@mail.ru>
2020-08-21 19:50:19 +0500

    Added user case models structure

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Oringik/nyan404-libs
+
+go 1.15
---
+++ models/user_case.go
@@ -0,0 +1,42 @@
+package models
+
+type job int
+
+const (
+	JOB_POLICEMAN job = iota
+	JOB_CIVIL
+)
+
+// UserCase ...
+type UserCase struct {
+	PictureID uint
+	UserInfo
+	Case
+}
+
+// Case ...
+type Case struct {
+	Description
+	Ans []Answer
+}
+
+// UserInfo ...
+type UserInfo struct {
+	Name    string
+	Surname string
+	Gender  string
+	Age     string
+	Job     job
+}
+
+// Description ...
+type Description struct {
+	Title string
+	Text  string
+}
+
+// Answer ...
+type Answer struct {
+	Text   string
+	Weight uint
+}
