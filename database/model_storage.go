a521a8d54d78687df729f3ddc6601d3407d914e9
Oringik <zikalol2@mail.ru>
2020-08-22 14:44:36 +0500

    Added getarray method in orm

--- database/model_storage.go
+++ database/model_storage.go
@@ -58,8 +58,76 @@ func (mos *ModelStorage) Equal(value interface{}) *ModelStorage {
 }
 
 // Update ...
-func (mos *ModelStorage) 
+func (mos *ModelStorage) Update(value interface{}) error {
+	mos.Lock()
+	defer mos.Unlock()
 
+	for _, value := range mos.storage.Data {
+		if reflect.TypeOf(value) == reflect.TypeOf(mos.model) {
+			localValue := reflect.ValueOf(value)
+
+			sValue := localValue.Elem()
+
+			fieldLocal := sValue.FieldByName(mos.field)
+
+			if sValue.Kind() != reflect.Struct {
+				return errors.New("Should be structure model")
+			}
+
+			switch fieldLocal.Kind() {
+			case reflect.Int:
+				valueCompareLocal := int(fieldLocal.Int())
+
+				if valueCompareLocal == mos.opts.equal.(int) {
+					if !fieldLocal.OverflowInt(int64(value.(int))) {
+						fieldLocal.SetInt(int64(value.(int)))
+						return nil
+					}
+				}
+			case reflect.Uint:
+				valueCompareLocal := uint(fieldLocal.Uint())
+
+				if reflect.TypeOf(mos.opts.equal).Kind() == reflect.Int {
+					valueCompare := mos.opts.equal.(int)
+
+					if valueCompareLocal == uint(valueCompare) {
+						if reflect.TypeOf(value).Kind() == reflect.Int {
+							valueSet := value.(int)
+
+							if !fieldLocal.OverflowInt(int64(valueSet)) {
+								fieldLocal.SetUint(uint64(valueSet))
+								return nil
+							}
+						}
+
+					}
+					continue
+				}
+
+				if valueCompareLocal == mos.opts.equal.(uint) {
+					valueSet := value.(uint)
+
+					if !fieldLocal.OverflowUint(uint64(valueSet)) {
+						fieldLocal.SetUint(uint64(valueSet))
+						return nil
+					}
+				}
+
+			case reflect.String:
+				valueCompareLocal := fieldLocal.String()
+
+				if valueCompareLocal == mos.opts.equal.(string) {
+					valueSet := value.(string)
+
+					fieldLocal.SetString(valueSet)
+				}
+			}
+
+		}
+	}
+
+	return errors.New("Value not found")
+}
 
 // Get ...
 func (mos *ModelStorage) Get() (interface{}, error) {
@@ -114,6 +182,65 @@ func (mos *ModelStorage) Get() (interface{}, error) {
 
 	return nil, errors.New("Value not found")
 
+}
+
+// GetArray ...
+func (mos *ModelStorage) GetArray() (interface{}, error) {
+	mos.Lock()
+	defer mos.Unlock()
+
+	var result []interface{}
+
+	for _, value := range mos.storage.Data {
+		if reflect.TypeOf(value) == reflect.TypeOf(mos.model) {
+			localValue := reflect.ValueOf(value)
+
+			sValue := localValue.Elem()
+
+			fieldLocal := sValue.FieldByName(mos.field)
+
+			if sValue.Kind() != reflect.Struct {
+				return nil, errors.New("Should be structure model")
+			}
+
+			switch fieldLocal.Kind() {
+			case reflect.Int:
+				valueCompareLocal := int(fieldLocal.Int())
+
+				if valueCompareLocal == mos.opts.equal.(int) || mos.opts.equal.(int) == 0 {
+					result = append(result, value)
+				}
+
+			case reflect.Uint:
+				valueCompareLocal := uint(fieldLocal.Uint())
+
+				if reflect.TypeOf(mos.opts.equal).Kind() == reflect.Int {
+					valueCompare := mos.opts.equal.(int)
+
+					if valueCompareLocal == uint(valueCompare) || mos.opts.equal.(uint) == 0 {
+						result = append(result, value)
+					}
+
+					continue
+				}
+
+				if valueCompareLocal == mos.opts.equal.(uint) || mos.opts.equal.(uint) == 0 {
+					result = append(result, value)
+				}
+
+			case reflect.String:
+				valueCompareLocal := fieldLocal.String()
+
+				if valueCompareLocal == mos.opts.equal.(string) || mos.opts.equal.(string) == "" {
+					result = append(result, value)
+				}
+
+			}
+
+		}
+	}
+
+	return nil, errors.New("Value not found")
 }
 
 // Set ...
