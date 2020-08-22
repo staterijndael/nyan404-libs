686d1a0ba15d5259f01ccbbe8e1a0cf6fff6bb4c
Oringik <zikalol2@mail.ru>
2020-08-23 00:22:50 +0500

    Job by string added

--- models/user_case.go
+++ models/user_case.go
@@ -4,28 +4,28 @@ type pic int
 type job string
 
 const (
-	PIC_ADD pic = iota
-	PIC_DOCTOR_GIRL
-	PIC_KID
-	PIC_REFEREE
-	PIC_COOK
-	PIC_DOCTOR_BOY
-	PIC_LAWYER
-	PIC_SECRETARY
-	PIC_KING
-	PIC_VAMPIR
-	PIC_TAXI
-	PIC_POLICEMAN_BOY
-	PIC_POLICEMAN_GIRL
-	PIC_QUEEN
-	PIC_DELETE
-	PIC_SINGER
-	PIC_SPY_BOY
-	PIC_SUPERMAN
-	PIC_SPY_GIRL
-	PIC_STRANGER_GIRL
-	PIC_SUPERGIRL
-	PIC_MINER
+	PIC_ADD            pic = 1
+	PIC_DOCTOR_GIRL        = 2
+	PIC_KID                = 3
+	PIC_REFEREE            = 4
+	PIC_COOK               = 5
+	PIC_DOCTOR_BOY         = 6
+	PIC_LAWYER             = 7
+	PIC_SECRETARY          = 8
+	PIC_KING               = 9
+	PIC_VAMPIR             = 10
+	PIC_TAXI               = 11
+	PIC_POLICEMAN_BOY      = 12
+	PIC_POLICEMAN_GIRL     = 13
+	PIC_QUEEN              = 14
+	PIC_DELETE             = 15
+	PIC_SINGER             = 16
+	PIC_SPY_BOY            = 17
+	PIC_SUPERMAN           = 18
+	PIC_SPY_GIRL           = 19
+	PIC_STRANGER_GIRL      = 20
+	PIC_SUPERGIRL          = 21
+	PIC_MINER              = 22
 )
 
 const (
